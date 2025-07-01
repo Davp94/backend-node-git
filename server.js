eb3c005088506a14136bf6e7ad19f2125cd5758b
david_blumbit <david.villca.pacheco@gmail.com>
2025-06-30 20:47:43 -0400

    restore changes server.js

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/saludo', (req, res)=> {
-    res.json({message: 'HOLA DESDE NODEJS'})
+app.get("/", (req, res) => {
+  res.json({ message: "WELCOME TO MY API" });
 });
 
-app.get('/estado', (req, res)=> {
-    res.json({status: 'SERVICE RUNNING'})
+app.get("/status", (req, res) => {
+  res.json({ status: "STATUS CHECK SUCCESFULY" });
 });
 
-app.get('/hello', (req, res)=> {
-    res.json({message: 'HELLO WORLD'})
+app.get("/products", (req, res) => {
+  res.json({
+    message: "PRODUCT LIST",
+    products: ["Product1", "Product2", "Product3"],
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.json({
+    message: "CONTACT INFO",
+    email: "soporte@miempresa.com",
+    phone: "+56 9 1234 5678",
+  });
 });
 
 app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
-})
+  console.log("Server is running on port", PORT);
+});
