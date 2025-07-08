e1adeffb286bc56e9557de044ee14a3e82f46f27
david_blumbit <david.villca.pacheco@gmail.com>
2025-07-08 19:50:15 -0400

    Revert "conflictos desde main"
    This reverts commit f1cc0e4ebbef52b2ad94e323a8d639087d077bd7.

--- server.js
+++ server.js
@@ -2,27 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.json({ message: "WELCOME TO MY API" });
+app.get('/hello', (req, res)=> {
+    res.json({message: 'HOLA MUNDO'})
 });
 
-app.get("/status", (req, res) => {
-  res.json({ status: "STATUS CHECK SUCCESFULY" });
-});
-
-app.get("/products", (req, res) => {
-  res.json({
-    message: "PRODUCT LIST",
-    products: ["Product1", "Product2", "Product3"],
-  });
-});
-
-app.get("/contact", (req, res) => {
-  res.json({
-    message: "CONTACT INFO",
-    email: "soporte@miempresa.com",
-    phone: "+56 9 1234 5678",
-  });
+app.get('/status', (req, res)=> {
+    res.json({message: 'SERVICE IS RUNNING'})
 });
 
 app.listen(PORT, () => {
