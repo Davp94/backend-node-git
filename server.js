e40eacef3a5c9fe23763622cd37c72b8444659d5
david_blumbit <david.villca.pacheco@gmail.com>
2025-07-08 21:05:21 -0400

    add docker compose and db connect

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const app = express();
+const { Pool } = require('pg');
 const PORT = process.env.PORT || 3000;
+
+//CREATE POSTGRES CONNECTION
+const pool = new Pool({
+  host: 'localhost',
+  user: 'postgres',
+  password: '123456',
+  database: 'supermercado_db',
+  port: 5432
+})
+
+app.get('/test-db', async (req, res)=> {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT NOW()");
+    client.release(); 
+    res.json({status: 'Database connected succesfull', result: result})
+  } catch (error) {
+    console.log('ERROR', error)
+    res.json({status: 'Database connect error'})
+  }
+});
 
 app.get('/hello', (req, res)=> {
     res.json({message: 'HOLA MUNDO'})
